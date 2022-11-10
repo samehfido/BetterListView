e2192c3c064a66bfef5a874c8290bf44c4dfa0a3
Coder007 <23367063+samehfido@users.noreply.github.com>
2022-11-10 23:00:21 +0200

    Add files via upload

---
+++ BetterListViewCheckBoxes.cs
@@ -0,0 +1,16 @@
+namespace ComponentOwl.BetterListView;
+
+/// <summary>
+///   Display mode of item check boxes
+/// </summary>
+public enum BetterListViewCheckBoxes
+{
+	/// <summary>
+	///   hide check boxes
+	/// </summary>
+	Hide = 0,
+	/// <summary>
+	///   show two-state check boxes
+	/// </summary>
+	TwoState = 1
+}
---
+++ BetterListViewDragDropType.cs
@@ -0,0 +1,9 @@
+namespace ComponentOwl.BetterListView;
+
+internal enum BetterListViewDragDropType
+{
+	External = 0,
+	Internal = 1,
+	ItemReorder = 2,
+	Undefined = 3
+}
